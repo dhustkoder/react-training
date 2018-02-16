e2c87c993b2c61c3b55bc255b9425baeb10854ee
Rafael Moura <rafaelmoura.dev@gmail.com>
2018-02-16 21:25:00 -0200

    project structure

---
+++ teste/src/boot/setup.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { Font } from 'expo';
+import App from "../App";
+
+
+export default class Setup extends Component {
+	render() {
+		return <App />;
+	}
+}
+
+
